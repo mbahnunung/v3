d3c1c7378b52510db3156b2321ea08f507c2c47c
mbahnunung <mbahnunung@users.noreply.github.com>
2020-03-13 14:11:32 +0700

    Add files via upload

---
+++ lbg-audio6-html5-shoutcast_history/audio6_html5_radio_history/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>mbah nunung Online</title>
+<link href="audio6_html5_radio_history/audio6_html5.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=PT+Sans%3A400%2C700&#038;ver=4.7.2" rel="stylesheet" type="text/css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+<!-- and new libraries to use lastfm -->
+<script src="audio6_html5_radio_history/js/lastfm.api.md5.js" type="text/javascript"></script>
+<script src="audio6_html5_radio_history/js/lastfm.api.js" type="text/javascript"></script>
+<script src="audio6_html5_radio_history/js/lastfm.api.cache.js" type="text/javascript"></script>
+<!-- and new libraries to use lastfm -->						
+<script src="audio6_html5_radio_history/js/jquery.mousewheel.min.js" type="text/javascript"></script>
+<script src="audio6_html5_radio_history/js/jquery.touchSwipe.min.js" type="text/javascript"></script>
+<script src="audio6_html5_radio_history/js/audio6_html5.js" type="text/javascript"></script>
+		
+</head>
+					<body style="padding:0px;margin:0px;"><script>		jQuery(function() {			jQuery("#lbg_audio6_html5_shoutcast_1").audio6_html5({				radio_stream:"http://x5.mbahnunungonline.net:8000/stream",				radio_name:"mbah nunung Online",				playerWidth:300,				imageHeight:300,				skin:"whiteControllers",				responsive:true,				grabLastFmPhoto:true,				autoPlay:true,				songTitleColor:"#ffffff",				authorTitleColor:"#ffffff",				lineSeparatorColor:"#00FFFF",				radioStationColor:"#ffffff",				frameBehindTextColor:"#000000",				frameBehindButtonsColor:"#454545",				sticky:false,				startMinified:false,				showOnlyPlayButton:false,				centerPlayer:true,				playerBorderSize:0,				playerBorderColor:"#000000",				showFacebookBut:true,				facebookAppID:"1362312347152239",				facebookShareTitle:"HTML5 Radio Player With History- Shoutcast and Icecast",				facebookShareDescription:"A top-notch responsive HTML5 Radio Player compatible with all major browsers and mobile devices.",				facebookShareImage:"https://1.bp.blogspot.com/-ICiPZfl3aBQ/VtXCSDPuLKI/AAAAAAAAFPg/1THrDCGG9iI/s1600/03.jpg",				showTwitterBut:true,				showVolume:true,				showRadioStation:true,				showTitle:true,				showHistoryBut:true,				showHistory:true,				showHistoryOnInit:true,				translateReadingData:"reading data...",				historyTranslate:"HISTORY - latest played songs",				historyTitleColor:"#858585",				historyBgColor:"#ebebeb",				historyRecordBgColor:"transparent",				historyRecordBottomBorderColor:"transparent",				historyRecordSongColor:"#000000",				historyRecordSongBottomBorderColor:"#d0d0d0",				historyRecordAuthorColor:"#6d6d6d",				numberOfThumbsPerScreen:2,				historyPadding:16,				historyRecordTitleLimit:28,				historyRecordAuthorLimit:36,				pathToAjaxFiles:"http://heroradioplayer.lambertgroupproductions.com/wp-content/plugins/lbg-audio6-html5-shoutcast_history/audio6_html5_radio_history/",				nowPlayingInterval:25,					noImageAvailable:"audio6_html5_radio_history/1d205655ef29e14a8255c89fe2383a41.jpg"			});		});	</script>	    <div class="audio6_html5">            <audio id="lbg_audio6_html5_shoutcast_1" preload="metadata">              No HTML5 audio playback capabilities for this browser. Use <a href="https://www.google.com/intl/en/chrome/browser/">Chrome Browser!</a>                </audio>          </div>             	
+</body>
+</html>
