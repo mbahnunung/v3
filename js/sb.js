e9b885a88a4702347cc7cd007249407f94353a31
mbahnunung <mbahnunung@users.noreply.github.com>
2025-07-16 20:02:54 +0700

    Update sb.js

--- js/sb.js
+++ js/sb.js
@@ -10,7 +10,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const RADIO_NAME = 'Suara Banyuwangi Live';
+const RADIO_NAME = 'Suara Banyuwangi FM Live';
 
 // Change Zeno Stream URL Here, .
 const URL_STREAMING = 'https://stream.zeno.fm/skk0a2d6sd0uv';
